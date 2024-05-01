534819f0b1d5cdb81304389039ac8151d2f7822a
Anshul Kumar Gupta <83653522+0x4nshu1@users.noreply.github.com>
2024-05-01 10:44:29 +0530

    Add files via upload

---
+++ RemoveTempFiles_modified.py
@@ -0,0 +1,55 @@
+import os
+import shutil
+
+
+def get_temp_folder(username):
+    return "C:\\Users\\{}\\AppData\\Local\\Temp".format(username)
+
+def print_temp_folder_content(temp_folder):
+    files = [f for f in os.listdir(temp_folder)]
+    print("Content in your temp folder:\n\n")
+    for item in files:
+        print(item)
+
+def delete_temp_folder(temp_folder):
+    try:
+        shutil.rmtree(temp_folder)
+    except PermissionError:
+        print("Some files are kept as some processes are using it")
+        print("Please close those applications or wait a bit and then run this script again.")
+        print("Other wise rest all the removed.\n\n")
+        return False
+    except FileNotFoundError:
+        print("No Temporary files exist!")
+        return False
+
+    return True
+
+def main():
+    print("Save your work and close all the programs.")
+    username = input("Enter your registered username on Windows: ")
+    temp_folder = get_temp_folder(username)
+
+    while True:
+        if not os.path.exists(temp_folder):
+            print("\nThe temp folder doesn't exist.")
+            choice = input("Type 'quit' to exit: ") 
+            if choice.lower() == 'quit':
+                exit()
+
+        else:
+            print_temp_folder_content(temp_folder)
+            print()
+            choice = input("Do you want to continue?[y/n]: ")
+            if choice.lower() == 'y':
+                success = delete_temp_folder(temp_folder)
+                if success:
+                    print("\nAll temporary files have been deleted from the temp folder.\n")
+                choice = input("Type 'quit' to exit: ") 
+                if choice.lower() == 'quit':
+                    exit()
+            else:
+                exit()
+
+if __name__ == "__main__":
+    main()
